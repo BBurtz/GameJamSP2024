43cedf2bde354f6dc5abd80ae16ba82517fec78d
Marissa Moser <marissalmoser@gmail.com>
2024-01-28 12:42:11 -0600

    audio manager

---
+++ GameJam2024/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager Instance;
+
+    void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+
+            s.source.clip = s.clip;
+            s.source.outputAudioMixerGroup = s.mixer;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = System.Array.Find(sounds, sounds => sounds.name == name);
+        s.source.Play();
+    }
+
+    public void Stop(string name)
+    {
+        Sound s = System.Array.Find(sounds, sounds => sounds.name == name);
+        s.source.Stop();
+    }
+}
